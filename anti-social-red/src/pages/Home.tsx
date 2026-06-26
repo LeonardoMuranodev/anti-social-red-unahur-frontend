321d703042b2afcc40cdadb99c559485d327bce1
Matias-Ezeq <matiasezequiel.delarosa@estudiantes.unahur.edu.ar>
2026-06-26 16:41:27 -0300

    maquetado básico

--- anti-social-red/src/pages/Home.tsx
+++ anti-social-red/src/pages/Home.tsx
@@ -1,6 +1,18 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 function Home() {
 
-  return (<></>)
+  return (
+  <Container>
+    <Row>
+      <Col>nav lateral</Col>
+      <Col sm={6} className=''>feed y demas cosas</Col>
+      <Col>icono de usuario</Col>
+    </Row>
+  </Container>
+  )
 }
 
 export default Home
