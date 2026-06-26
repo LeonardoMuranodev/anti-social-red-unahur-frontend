2fb4088bc29e83c5729163f32215ccee85c7913c
Matias-Ezeq <matiasezequiel.delarosa@estudiantes.unahur.edu.ar>
2026-06-26 14:42:19 -0300

    feat crear rutas y páginas vacias

--- anti-social-red/src/App.tsx
+++ anti-social-red/src/App.tsx
@@ -1,7 +1,21 @@
+import { Route, Routes } from "react-router";
+import Home from "./pages/Home";
+import Welcome from "./pages/Welcome";
+import Profile from "./pages/Profile";
+import Error404 from "./pages/Error404";
+import PostDetail from "./pages/PostDetail";
+
+
 function App() {
-  return (
-    <>
-      <p>ToDo: página de inicio (sin login)</p>
+
+  return (<>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/welcome' element={<Welcome/>}/>
+      <Route path='/user/*' element={<Profile/>}/>
+      <Route path='/post/*' element={<PostDetail/>}/>
+      <Route path='/*' element={<Error404/>}/>
+    </Routes>
     </>
   )
 }
