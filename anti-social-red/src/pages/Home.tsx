0a52c0b08bc9ec07b3bd45e3478cb4d42bb5b822
Matias-Ezeq <matiasezequiel.delarosa@estudiantes.unahur.edu.ar>
2026-06-26 17:35:02 -0300

    maquetado pagina de inicio

--- anti-social-red/src/pages/Home.tsx
+++ anti-social-red/src/pages/Home.tsx
@@ -4,14 +4,16 @@ import Col from 'react-bootstrap/Col';
 
 function Home() {
 
-  return (
+  return (<>
   <Container>
     <Row>
-      <Col>nav lateral</Col>
-      <Col sm={6} className=''>feed y demas cosas</Col>
-      <Col>icono de usuario</Col>
+      <Col xs={{order:5}} classname='mainContent'>nav lateral</Col>
+      <Col sm={10} md={8} classname='mainContent'>feed y demas cosas</Col>
+      <Col classname='mainContent'>icono de usuario</Col>
     </Row>
   </Container>
+  <div className='nav-mobile'>nav mobile</div>
+  </>
   )
 }
 
