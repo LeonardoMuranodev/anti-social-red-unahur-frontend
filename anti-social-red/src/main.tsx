d0a902071e3b3c411282320c5f8e1c171fb3ffb2
Matias-Ezeq <matiasezequiel.delarosa@estudiantes.unahur.edu.ar>
2026-06-26 22:50:16 -0300

    estilizado landing page

--- anti-social-red/src/main.tsx
+++ anti-social-red/src/main.tsx
@@ -6,8 +6,6 @@ import 'bootstrap/dist/css/bootstrap.css';
 import './index.css'
 
 import App from './App.tsx'
-import { BrowserRouter } from 'react-router'
-
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
--- anti-social-red/src/pages/Welcome.tsx
+++ anti-social-red/src/pages/Welcome.tsx
@@ -1,13 +1,13 @@
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Dumpster from '../assets/hero.png'
+import Dumpster from '../assets/hero.png';
 
 function Welcome() {
   return (
-    <Container fluid>
+    <Container fluid className='landing-container'>
       <Row>
-        <Col>
+        <Col className='landing-text'>
          <h1>
             Acá nadie se salva.
          </h1>
@@ -20,38 +20,39 @@ function Welcome() {
          </h2>
          <button>Vení y decilo de frente</button>
         </Col>
-        <Col sm={4} className='d-none d-sm-flex justify-content-start'>
+        <Col sm={5} className='landing-image d-none d-sm-block'>
             <img src={Dumpster} className='img-fluid object-fit-contain'/>
         </Col>
       </Row>
       <br/>
-      <Row>
-        <Col sm={4}>
+      <Row className='row-even'>
+        <Col sm={5} className='landing-image'>
          captura de un post
         </Col>
-        <Col>
-        <h2>
-            Sin anuncios ni seguimiento
-        </h2>
-        <p>
-            ninguna empresa en su sano juicio va a querer vincular su nombre con nosotros.
-        </p>
-        <h2>
-            Sin Algoritmos
-        </h2>
-        <p>
-            nuestras controversias son 100% naturales
-        </p>
-        <h2>
-            Sin subscripciones
-        </h2>
-        <p>
-            Nuestro modelo de negocio de ultima generación nos permite mantener el sitio siempre online el 30% de las veces.
-        </p>
+        <Col className='landing-text'>
+          <h2>
+              Sin anuncios ni seguimiento
+          </h2>
+          <p>
+              ninguna empresa en su sano juicio va a querer vincular su nombre con nosotros.
+          </p>
+          <h2>
+              Sin Algoritmos
+          </h2>
+          <p>
+              nuestras controversias son 100% naturales
+          </p>
+          <h2>
+              Sin subscripciones
+          </h2>
+          <p>
+              Nuestro modelo de negocio de ultima generación nos permite mantener el sitio siempre online el 30% de las veces.
+          </p>
         </Col>
       </Row>
+      <br/>
       <Row>
-        <Col>
+        <Col className='landing-text'>
           <h1>
             Nuestros ideales
           </h1>
@@ -66,17 +67,21 @@ function Welcome() {
           <p>Garantizamos que todos nuestros usuarios son humanos, toxicidad 100% orgánica libre de gluten.</p>
         </Col>
       </Row>
-      <Row>
-        <Col>
+      <br/>
+      <Row className='row-even'>
+        <Col className='landing-text'>
+          <h1>Y a todo esto... ¿quien sos vos?</h1>
           <p>antiSocial es un producto de softWorse Co., una compañia de software que esta harta de las boludeces que hacen las otras redes. Ningún billonario (ni trillonario) va a comprarnos y hacer lo que se le cante con nuestros productos, arruinándolos en el proceso. No nos hacemos responsables por lo que hagas y dejes de hacer en nuestro sitio.</p>
         </Col>
       </Row>
+      <br/>
       <Row>
-        <Col>
+        <Col className='landing-text'>
           <h1>¿Listo para mandar todo a la #$&*%?</h1>
           <button>Unirse al bardo</button>
         </Col>
       </Row>
+      <br/>
       <Row>footer</Row>
     </Container>
   )
