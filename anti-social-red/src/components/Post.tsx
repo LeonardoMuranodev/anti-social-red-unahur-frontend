d63df80cfe94c1a77571f72b5800980ff525c5fc
Matias-Ezeq <matiasezequiel.delarosa@estudiantes.unahur.edu.ar>
2026-06-28 17:32:25 -0300

    estilizado basico de pagina principal

---
+++ anti-social-red/src/components/Post.tsx
@@ -0,0 +1,10 @@
+
+export default function Post({id}) {
+  return(
+    <div className='border bg-secondary p-2 flex flex-column'>
+      <h2>placeholder post {id}</h2>
+      <p>@autor</p>
+      <p>placeholder de post</p>
+    </div>
+  );
+}
--- anti-social-red/src/pages/Home.tsx
+++ anti-social-red/src/pages/Home.tsx
@@ -1,15 +1,54 @@
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import { Button, ButtonGroup, Form, InputGroup, Stack } from 'react-bootstrap';
+import Post from '../components/Post';
 
 function Home() {
 
   return (<>
   <Container fluid>
     <Row>
-      <Col className='main-content d-none d-sm-block'>nav lateral</Col>
+      <Col className='main-content d-none d-sm-block bg-primary'><p>nav lateral</p></Col>
+      <Col sm={8} className='main-content p-2'>
+        <Stack gap={2}>
+          <InputGroup>
+            <Form.Control
+              placeholder="Buscar"
+              aria-label="Buscar"
+              aria-describedby="basic-addon2"
+            />
+            <Button variant="secondary" id="button-addon2">
+              B
+            </Button>
+          </InputGroup>
+          <Form>
+            <Form.Group className="mb-3" controlId="">
+              <Form.Control
+                placeholder="Título"
+                aria-label="Titulo"
+              />            
+              <Form.Control as="textarea" placeholder="Redactar publicación" rows={3} />
+              <ButtonGroup>
+                <Button variant='secondary border'>Agregar Imagen</Button>
+                <Button variant='secondary border'>Archivar</Button>
+                <Button variant='primary border'>Publicar</Button>
+              </ButtonGroup>
+            </Form.Group>
+          </Form>
+          <Stack gap={1}>
+            <ButtonGroup>
+              <Button variant='primary border'>Mi Feed</Button>
+              <Button variant='secondary border'>Feed Global</Button>
+            </ButtonGroup>
+            <Post id={1}/>
+            <Post id={2}/>
+            <Post id={3}/>
+            <Post id={4}/>
+          </Stack>
+        </Stack>
+      </Col>
       <Col className='main-content xs-order-first'>pfp</Col>
-      <Col sm={8} className='main-content'>feed y demas cosas</Col>
     </Row>
   </Container>
   <div className='nav-mobile d-sm-none'>nav mobile</div>
