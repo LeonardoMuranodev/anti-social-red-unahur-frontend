9c3358bad794f8762c8b995c4b38ee42f22ef26e
Matias-Ezeq <matiasezequiel.delarosa@estudiantes.unahur.edu.ar>
2026-06-29 19:17:25 -0300

    feat: estilizado basico de perfil de usuario

--- front/src/pages/Profile.tsx
+++ front/src/pages/Profile.tsx
@@ -1,9 +1,24 @@
+import { useContext } from "react";
+import { AuthContextGlobal } from "../context/AuthContext";
+import { Button, Col, Container, Row, Stack } from "react-bootstrap";
+
 function Profile() {
 
+  const {user, logout} =  useContext(AuthContextGlobal);
+
   return (
-  <>
-    <p>User</p>
-  </>
+    <Container fluid>
+      <Row className="justify-content-center">
+        <Col sm={8} className="bg-primary">
+          <Stack>
+            <h3>{user['nickname']}</h3>
+            <p>Seguidores: {user['seguidores'].length}</p>
+            <p>Seguidos: {user['seguidos'].length}</p>
+            <Button onClick={logout} variant='danger border'>Cerrar sesión</Button>
+          </Stack>
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
