66e148ef2dc0e20b9264047b90d32b7077ee106d
Matias-Ezeq <matiasezequiel.delarosa@estudiantes.unahur.edu.ar>
2026-07-02 17:27:08 -0300

    maquetado form post

--- front/src/components/PostForm.tsx
+++ front/src/components/PostForm.tsx
@@ -5,13 +5,16 @@ export default function FormPost() {
         <Form>
                 <Form.Group className="mb-3" controlId="">
                   <Form.Control
+                    type="text"
+                    size="lg"
                     placeholder="Título"
                     aria-label="Titulo"
                   />
                   <Form.Control as="textarea" placeholder="Redactar publicación" rows={3} />
+                  <Form.Label>Tags</Form.Label>
+                  <Form.Control size="sm" type="text" placeholder="tags (separadas por comas)" />
                   <ButtonGroup>
                     <Button variant='secondary border'>Agregar Imagen</Button>
-                    <Button variant='secondary border'>Archivar</Button>
                     <Button variant='primary border'>Publicar</Button>
                   </ButtonGroup>
                 </Form.Group>
